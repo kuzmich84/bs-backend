fe9745fc952c89f010d4a9a8fe6640c59aaee165
Dmitry Kuchukov <kudim@inbox.ru>
2022-05-20 04:43:11 +0300

    delete empty content type

--- src/api/school/controllers/school.js
+++ src/api/school/controllers/school.js
@@ -6,4 +6,17 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::school.school');
+module.exports = createCoreController('api::school.school', ({strapi})=> ({
+
+  async create(ctx) {
+    let entity;
+    ctx.request.body.owner = ctx.state.user.id
+    console.log()
+    entity = await strapi.service('api::school:school').create(ctx.request.body)
+
+    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+    return this.transformResponse(sanitizedEntity);
+
+  }
+
+}));
