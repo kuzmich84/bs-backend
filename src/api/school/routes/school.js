14acc1d8f3a6ca6ae97f1868fd9f9810f815496b
Kuchukov Dmitry <kudim@inbox.ru>
2022-06-03 14:18:26 +0300

    add policies is-owner in routes school

--- src/api/school/routes/school.js
+++ src/api/school/routes/school.js
@@ -1,9 +1,18 @@
-'use strict';
+'use strict'
 
 /**
  * school router.
  */
 
-const { createCoreRouter } = require('@strapi/strapi').factories;
+const {createCoreRouter} = require('@strapi/strapi').factories
 
-module.exports = createCoreRouter('api::school.school');
+module.exports = createCoreRouter('api::school.school', {
+  config: {
+    delete: {
+      policies: ['api::school.is-owner']
+    },
+    update: {
+      policies: ['api::school.is-owner']
+    }
+  }
+})
