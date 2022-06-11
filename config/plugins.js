0b1e3ce64ecf553948d72d44d05e7f22191a3ed4
Kuchukov Dmitry <kudim@inbox.ru>
2022-06-11 23:54:58 +0300

    refactoring

--- config/plugins.js
+++ config/plugins.js
@@ -17,5 +17,20 @@ module.exports = ({env}) => ({
       },
     },
   },
+  upload: {
+    provider: 'aws-s3',
+    providerOptions: {
+      accessKeyId: env('AWS_ACCESS_KEY_ID'),
+      secretAccessKey: env('AWS_ACCESS_SECRET'),
+      endpoint: 'https://s3.storage.selcloud.ru',
+      apiVersion: 'latest',
+      region: env('AWS_REGION'),
+      params: {
+        Bucket: env('AWS_BUCKET_NAME'),
+      },
+      logger: console
+    }
+
+  }
 
 })
